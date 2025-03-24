5c7ab11ab3bf558e69b6c92ab82b53433d8de5a9
nazarovman2024 <nazarovman2024@gmail.com>
2025-03-24 05:18:50 +0300

    Initial commit

---
+++ src/test/java/ru/nazarov/man/pre_project/PreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nazarov.man.pre_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
