09135729bedd07f6dc51f5b1b646d1569d5c1242
nazarovman <nazarovman2024@ya.ru>
2025-04-04 14:13:16 +0300

    20250404_0.3a_newUser

--- src/main/java/ru/nazarov/man/pre_project/validators/FieldsValueMatch.java
+++ src/main/java/ru/nazarov/man/pre_project/validators/FieldsValueMatch.java
@@ -1,20 +1,24 @@
 package ru.nazarov.man.pre_project.validators;
 
 import jakarta.validation.Constraint;
-
+import jakarta.validation.Payload;
 import java.lang.annotation.*;
 
 @Constraint(validatedBy = FieldsValueMatchValidator.class)
-@Target({ ElementType.TYPE })
+@Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface FieldsValueMatch {
-
     String message() default "Fields values don't match!";
 
+    // Обязательные параметры для валидации
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+
+    // Кастомные параметры
     String field();
     String fieldMatch();
 
-    @Target({ ElementType.TYPE })
+    @Target({ElementType.TYPE})
     @Retention(RetentionPolicy.RUNTIME)
     @interface List {
         FieldsValueMatch[] value();
