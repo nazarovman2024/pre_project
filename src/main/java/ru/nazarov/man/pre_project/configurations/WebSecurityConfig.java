beed92b5c233c732f52e1ee1579ef809441c1989
nazarovman <nazarovman2024@ya.ru>
2025-04-09 17:29:07 +0300

    PP_3.1.4_v1.1_PreAuthorize-fix

--- src/main/java/ru/nazarov/man/pre_project/configurations/WebSecurityConfig.java
+++ src/main/java/ru/nazarov/man/pre_project/configurations/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package ru.nazarov.man.pre_project.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
@@ -12,6 +13,7 @@ import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
 
     private final SuccessUserHandler successUserHandler;
